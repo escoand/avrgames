0e582c43f04ab42ef611a796c9f7457a90a97f98
escoand <escoand@users.noreply.github.com>
2015-03-04 09:18:49 +0100

    more generic output

--- tetris.c
+++ tetris.c
@@ -5,6 +5,7 @@
 #include <conio.h>
 #include <windows.h>
 #define usleep(x) Sleep(x / 1000)
+#elif __AVR__
 #else
 #include <string.h>
 #include <termios.h>
@@ -13,29 +14,30 @@
 #endif
 
 #include "tetris.h"
-char board[TETRIS_BOARD_WIDTH][TETRIS_BOARD_HEIGHT];
-char brick[TETRIS_BRICK_SIZE][TETRIS_BRICK_SIZE];
-int tick = 200;
-char
-canBeAt (int offset_x, int offset_y)
-{
-  int x;
-  int y;
+unsigned long board[TETRIS_BOARD_WIDTH][TETRIS_BOARD_HEIGHT];
+unsigned long brick[TETRIS_BRICK_SIZE][TETRIS_BRICK_SIZE];
+unsigned int tick = 200;
 
-  /*horizontal borders     */
+char
+canBeAt (signed short offset_x, signed short offset_y)
+{
+  unsigned short x;
+  unsigned short y;
+
+  /* horizontal borders */
   for (y = 0; y < TETRIS_BRICK_SIZE; y++)
     if (offset_x < 0 && brick[-1 - offset_x][y]
 	|| offset_x > TETRIS_BOARD_WIDTH - TETRIS_BRICK_SIZE + 1
 	&& brick[TETRIS_BOARD_WIDTH - offset_x][y])
       return 0;
 
-  /* vertical borders     */
+  /* vertical borders */
   for (x = 0; x < TETRIS_BRICK_SIZE; x++)
     if (offset_y > TETRIS_BOARD_HEIGHT - TETRIS_BRICK_SIZE
 	&& brick[x][TETRIS_BOARD_HEIGHT - offset_y])
       return 0;
 
-  /*former bricks     */
+  /* former bricks */
   for (x = 0; x < TETRIS_BRICK_SIZE; x++)
     {
       if (offset_x + x < 0 || offset_x + x >= TETRIS_BOARD_WIDTH)
@@ -50,15 +52,17 @@ canBeAt (int offset_x, int offset_y)
 	    return 0;
 	}
     }
+
   return 1;
 }
 
 void
 rotate (char clockwise)
 {
-  int x;
-  int y;
-  char tmp[TETRIS_BRICK_SIZE][TETRIS_BRICK_SIZE];
+  unsigned short x;
+  unsigned short y;
+  unsigned long tmp[TETRIS_BRICK_SIZE][TETRIS_BRICK_SIZE];
+
   for (x = 0; x < TETRIS_BRICK_SIZE; x++)
     for (y = 0; y < TETRIS_BRICK_SIZE; y++)
       {
@@ -68,14 +72,16 @@ rotate (char clockwise)
 	else
 	  tmp[y][TETRIS_BRICK_SIZE - x - 1] = brick[x][y];
       }
+
   memcpy (brick, tmp, sizeof (brick));
 }
 
 void
-insert (int offset_x, int offset_y, char reverse)
+insert (signed short offset_x, signed short offset_y, char reverse)
 {
-  int x;
-  int y;
+  unsigned short x;
+  unsigned short y;
+
   for (x = 0; x < TETRIS_BRICK_SIZE; x++)
     {
       if (offset_x + x < 0)
@@ -95,10 +101,11 @@ insert (int offset_x, int offset_y, char reverse)
 }
 
 void
-print ()
+output ()
 {
-  int x;
-  int y;
+  unsigned short x;
+  unsigned short y;
+
   for (y = 0; y < TETRIS_BOARD_HEIGHT; y++)
     {
       printf ("| ");
@@ -108,110 +115,110 @@ print ()
 	}
       printf ("|\n");
     }
+
   printf (">---------------------<\n");
 }
 
 void
 initOutput ()
 {
-
 #ifdef _WIN32
   DWORD mode;
   HANDLE hstdin;
   DWORD res;
+
   hstdin = GetStdHandle (STD_INPUT_HANDLE);
   GetConsoleMode (hstdin, &mode);
   SetConsoleMode (hstdin, 0);
-
 #else
   struct termios mode;
+
   tcgetattr (STDIN_FILENO, &mode);
   mode.c_lflag &= ~(ICANON | ECHO);
   tcsetattr (STDIN_FILENO, TCSANOW, &mode);
-
 #endif
-} int
+}
 
+int
 getkey ()
 {
-  char buf[128];
-  int len;
-  char ch = ' ';
+  unsigned char buf[128];
+  unsigned char len;
+  unsigned char ch = ' ';
 
 #ifdef _WIN32
   if (kbhit ())
-
 #else
   struct timeval tv;
   fd_set fds;
+
   tv.tv_sec = 0;
   tv.tv_usec = 0;
   FD_ZERO (&fds);
   FD_SET (STDIN_FILENO, &fds);
+
   select (STDIN_FILENO + 1, &fds, NULL, NULL, &tv);
   if (FD_ISSET (STDIN_FILENO, &fds))
-
 #endif
     {
       len = read (STDIN_FILENO, buf, sizeof (buf) - 1);
       ch = buf[len - 1];
     }
+
   return ch;
 }
 
 int
 main (void)
 {
-  char bit;
-  int x, y, offset_x, offset_y;
-  char key = ' ';
+  unsigned short x;
+  unsigned short y;
+  signed short offset_x;
+  signed short offset_y;
+  unsigned char rand_brick;
+  unsigned char rand_output;
+  unsigned char key;
+
   memset (board, TETRIS_BIT_EMPTY, sizeof (board));
   initOutput ();
+
   while (1)
     {
       offset_x = (TETRIS_BOARD_WIDTH - TETRIS_BRICK_SIZE) / 2;
       offset_y = -TETRIS_BRICK_SIZE + 1;
 
-      /* new brick         */
-      bit = TETRIS_BIT_CONSOLE[rand () %
-			       (sizeof (TETRIS_BIT_CONSOLE) / sizeof (char))];
-      memcpy (brick,
-	      TETRIS_BRICKS[rand () %
-			    (sizeof (TETRIS_BRICKS) /
-			     sizeof (char
-				     [TETRIS_BRICK_SIZE]
-				     [TETRIS_BRICK_SIZE]))],
-	      sizeof (char[TETRIS_BRICK_SIZE][TETRIS_BRICK_SIZE]));
+      /* new brick */
+      rand_brick = rand () % (sizeof (TETRIS_BIT_OUTPUT) / sizeof (long));
+      rand_output = rand () % (sizeof (TETRIS_BIT_OUTPUT) / sizeof (long));
+      memset (brick, 0, sizeof (brick));
       for (x = 0; x < TETRIS_BRICK_SIZE; x++)
 	for (y = 0; y < TETRIS_BRICK_SIZE; y++)
-	  if (brick[x][y])
-	    brick[x][y] = bit;
+	  if (TETRIS_BRICKS[rand_brick][x][y])
+	    brick[x][y] = TETRIS_BIT_OUTPUT[rand_output];
 
-      /*game over         */
+      /* game over */
       if (!canBeAt (offset_x, offset_y))
 	{
 	  printf (" !!! GAME OVER !!!  ");
 	  return EXIT_FAILURE;
 	}
+
       for (; offset_y <= TETRIS_BOARD_HEIGHT; offset_y++)
 	{
 	  insert (offset_x, offset_y - 1, 1);
 
-	  /*key actions            */
+	  /* key actions */
 	  key = getkey ();
 	  if (key == 'a' && canBeAt (offset_x - 1, offset_y))
 	    offset_x--;
-
 	  else if (key == 'd' && canBeAt (offset_x + 1, offset_y))
 	    offset_x++;
-
 	  else if (key == 'e')
 	    {
 	      rotate (1);
 	      if (!canBeAt (offset_x, offset_y))
 		rotate (0);
 	    }
-
 	  else if (key == 'q')
 	    {
 	      rotate (0);
@@ -219,16 +226,18 @@ main (void)
 		rotate (1);
 	    }
 
-	  /*go down        */
+	  /* go down */
 	  if (!canBeAt (offset_x, offset_y))
 	    {
 	      insert (offset_x, offset_y - 1, 0);
 	      break;
 	    }
+
 	  insert (offset_x, offset_y, 0);
-	  print ();
+	  output ();
 	  usleep (tick * 1000);
 	}
     }
+
   return EXIT_SUCCESS;
 }
--- tetris.h
+++ tetris.h
@@ -1,17 +1,23 @@
 #define TETRIS_BOARD_WIDTH  10
 #define TETRIS_BOARD_HEIGHT 18
 #define TETRIS_BRICK_SIZE   4
-#define TETRIS_BIT_EMPTY    ' '
+#define TETRIS_BIT_EMPTY    0
 
-char TETRIS_BIT_CONSOLE[] = {
+#ifdef __AVR__
+#define TETRIS_BIT_OUTPUT TETRIS_BIT_COLOR
+#else
+#define TETRIS_BIT_OUTPUT TETRIS_BIT_CONSOLE
+#endif
+
+unsigned long TETRIS_BIT_CONSOLE[] = {
   0x23, 0x24, 0x3d, 0x40, 0x4f, 0x7e
 };
 
-int TETRIS_BIT_COLOR[] = {
+unsigned long TETRIS_BIT_COLOR[] = {
   0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0xFF00FF, 0x00FFFF
 };
 
-char TETRIS_BRICKS[][TETRIS_BRICK_SIZE][TETRIS_BRICK_SIZE] = {
+unsigned long TETRIS_BRICKS[][TETRIS_BRICK_SIZE][TETRIS_BRICK_SIZE] = {
   {
    {0, 0, 0, 0},
    {0, 0, 0, 0},
