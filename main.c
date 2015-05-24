be6edf72939e9bb1fcfef485af440207c0214aec
escoand <escoand@users.noreply.github.com>
2015-05-24 11:53:33 +0200

    fixed wrong include

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
+#ifdef __AVR__
 #include <avr/io.h>
+#endif
 #include "tetris.h"
 
 int
 main (void)
 {
 #ifdef CLKPR
-  CLKPR = _BV(CLKPCE);
+  CLKPR = _BV (CLKPCE);
   CLKPR = 0;
 #endif
   return tetris_main ();
