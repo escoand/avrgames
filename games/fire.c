b0e571cf8ccfb48d1687b99ca148071a253791d3
escoand <escoand@users.noreply.github.com>
2018-11-21 16:44:14 +0100

    optimize fire

--- games/fire.c
+++ games/fire.c
@@ -9,6 +9,7 @@
 void
 fire_main(void)
 {
+    uint8_t x, y;
     board_matrix    board;
     memset(&board, 0, sizeof(board));
 
@@ -17,32 +18,24 @@ fire_main(void)
     getInput();
 
     while (getInput() == BUTTON_NONE) {
-	for (uint8_t x = 0; x < BOARD_WIDTH; x++) {
+	for (x = 0; x < BOARD_WIDTH; x++) {
 
-	    // Step 1.  Cool down every cell a little
-	    for (uint8_t y = 0; y < BOARD_HEIGHT; y++) {
+	    // cool down
+	    for (y = 0; y < BOARD_HEIGHT; y++) {
 		uint8_t         tmp =
 		    rand() % (FIRE_COOLING * 10 / BOARD_HEIGHT + 2);
 		board[y][x] = tmp < board[y][x] ? board[y][x] - tmp : 0;
 	    }
 
-	    // Step 2.  Heat from each cell drifts 'up' and diffuses a
-	    // little
-	    for (uint8_t y = BOARD_HEIGHT - 1; y >= 2; y--) {
+	    // drift up
+	    for (y = BOARD_HEIGHT - 1; y >= 2; y--) {
 		board[y][x] =
 		    (board[y - 1][x] + board[y - 2][x] +
 		     board[y - 2][x]) / 3;
 	    }
 
-	    // Step 3.  Randomly ignite new 'sparks' of heat near the
-	    // bottom
-	    if (rand() % 256 < FIRE_SPARKING) {
-		uint8_t         y = rand() % 8;
-		uint8_t         tmp = 160 + (rand() % 96);
-		board[y][x] =
-		    tmp + board[y][x] < 240 ? board[y][x] + tmp : 240;
-	    }
-
+	    // new fire
+	    board[0][x] = FIRE_BURN_MIN + (rand() % (255 - FIRE_BURN_MIN));
 	}
 
 	setOutputUsePalette(&board, BOARD_PALETTE_FIRE);
--- games/fire.h
+++ games/fire.h
@@ -2,7 +2,7 @@
 #define FIRE_H
 
 #define FIRE_TICK_MS  150
-#define FIRE_SPARKING 85
+#define FIRE_BURN_MIN 180
 #define FIRE_COOLING  120
 
 void            fire_main(void);
