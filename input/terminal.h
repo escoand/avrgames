16d0edb3e1c874672103f128d6bab77b67a00625
ans <andreas.schoenfelder@vwgis.de>
2019-05-24 09:31:50 +0200

    fix function name

--- input/terminal.h
+++ input/terminal.h
@@ -1,7 +1,9 @@
 #ifndef INPUT_TERMINAL_H
 #define INPUT_TERMINAL_H
 
+#include <stdint.h>
+
 void terminal_init(void);
-uint32_t terminal_loop(void);
+uint32_t terminal_getkey(void);
 
 #endif				/* INPUT_TERMINAL_H */
